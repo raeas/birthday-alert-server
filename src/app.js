a2643790910f2fe32fbf09f9138e65e535f1053a
Rae Schneider <rannschneid@gmail.com>
2021-03-14 11:25:08 -0600

    refactor for logging and error handling

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const errorHandler = require('./error-handler')
 const { NODE_ENV } = require('./config');
 const {CLIENT_ORIGIN} = require('./config');
 
@@ -23,15 +24,6 @@ app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/birthday-alert',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/birthday-alert-test',
+  API_TOKEN: process.env.API_TOKEN || 'development',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' 
 }
