91a43d6449b713ab0badee65c47038c993b35705
Rae Schneider <rannschneid@gmail.com>
2021-03-14 14:31:41 -0600

    set up migration scripts and seed data

---
+++ seeds/seed.people.sql
@@ -0,0 +1,12 @@
+insert into people (first_name, last_name, birthday)
+values
+  ('Hattie', 'Greene', '1986-03-14'),
+  ('Sheila', 'Munoz', '1965-04-04'),
+  ('Carrie', 'Patton', '1972-05-28'),
+  ('Anita', 'Carlson', '1993-06-26'),
+  ('Ollie', 'Guzman', '1947-07-30'),
+  ('Dwight', 'Berry', '2006-08-12'),
+  ('Jan', 'Austin', '2010-09-16'),
+  ('Greg', 'Rivera', '2016-10-17'),
+  ('Delores', 'Garrett', '2004-11-24'),
+  ('Charlotte', 'Jackson', '1996-01-26');
