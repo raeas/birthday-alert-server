48ddc0b484b2f4e044e9405545ac4e35fd5dee55
Rae Schneider <rannschneid@gmail.com>
2021-03-14 14:50:34 -0600

    added db connection info and knex to server file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
+//this is the controller file- it sets up the connection to the db
+const knex = require('knex')
 const app = require('./app')
 
-const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
