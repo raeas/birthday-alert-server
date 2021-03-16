67e3a9c23a67450b2908c1611de965b93fe77412
Rae Schneider <rannschneid@gmail.com>
2021-03-15 19:06:45 -0600

    added fixture array for people

---
+++ test/people-fixtures.js
@@ -0,0 +1,30 @@
+function makePeopleArray() {
+  return [
+    {
+      id: 1,
+      first_name: "Martin",
+      last_name: "Bryant",
+      birthday: "1975-09-25"
+    },
+    {
+      id: 2,
+      first_name: "Jonathan",
+      last_name: "Morris",
+      birthday: "1993-12-31"
+    },
+    {
+      id: 3,
+      first_name: "Melissa",
+      last_name: "Baker",
+      birthday: "2001-06-11"
+    },
+    {
+      id: 4,
+      first_name: "Howard",
+      last_name: "Phillips",
+      birthday: "1966-02-01"
+    }
+  ]
+}
+
+module.exports = { makePeopleArray }
