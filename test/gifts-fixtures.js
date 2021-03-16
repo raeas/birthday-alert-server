3785cac407be5406d892a4362fef5b4ca2060d65
Rae Schneider <rannschneid@gmail.com>
2021-03-15 19:13:26 -0600

    added fixtures array for gifts

---
+++ test/gifts-fixtures.js
@@ -0,0 +1,51 @@
+function makeGiftsArray() {
+  return [
+    {
+      id: 1,
+      gift_name: "Hula hoop",
+      person: 1
+    },
+    {
+      id: 2,
+      gift_name: "Sidewalk chalk",
+      person: 2
+    },
+    {
+      id: 3,
+      gift_name: "Earrings",
+      person: 3
+    },
+    {
+      id: 4,
+      gift_name: "T-shirt",
+      person: 4
+    },
+    {
+      id: 5,
+      gift_name: "Chocolates",
+      person: 1
+    },
+    {
+      id: 6,
+      gift_name: "Gift Card",
+      person: 2
+    },
+    {
+      id: 7,
+      gift_name: "Beach towel",
+      person: 3
+    },
+    {
+      id: 8,
+      gift_name: "Magnets",
+      person: 4
+    },
+    {
+      id: 9,
+      gift_name: "Lint roller",
+      person: 1
+    }
+  ]
+}
+
+module.exports = { makeGiftsArray }
