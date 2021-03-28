ed3b4c142e073dc6c2db16162e0730fb5860eb98
Rae Schneider <rannschneid@gmail.com>
2021-03-28 15:02:07 -0600

    config changes for deploy

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/birthday-alert',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/birthday-alert-test',
+  DATEBASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/birthday-alert',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/birthday-alert-test',
   API_TOKEN: process.env.API_TOKEN || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' 
 }
